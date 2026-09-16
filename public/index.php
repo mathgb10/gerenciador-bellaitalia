cd29401a227dfa5f4870957fa4cf4ecb92e55b8f
Matheus Benevides <matheusgoncalvesbenevides@gmail.com>
2026-09-15 22:01:46 -0300

    feat: tela de em construção

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bella Italia - Em construção</title>
+    <link rel="stylesheet" href="css/build.css">
+</head>
+<body>
+    <main>
+        <section>
+            <header>
+                <h1>Bella Italia - Painel</h1>
+                <h2>Em Construção</h2>
+            </header>
+            <div>
+                <p>Painel para controles de pedidos, mesas e dashboard da Bella Italia.</p>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
